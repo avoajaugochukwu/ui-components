5335504422964ce5b36b8e4850e1c54c9ef003a2
avoajaugochukwu <karlthegoo@hotmail.com>
2021-07-01 10:00:55 +0100

    Updated Box

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,13 +6,16 @@ function HomeScreen() {
       <div className="px-10 mx-10">
         <div><h1 className="text-4xl text-center my-10">Figma to HTML - Pixel perfect</h1></div>
         <div className="flex px-10 mx-10 flex-wrap">
-          <Box bgColor={true} header={'Tralfagar virtual healthcare'} month={'JUN'} day={4} url={'Trafalgar'} />
-          <Box bgColor={false} />
-          <Box bgColor={true} />
         
-          <Box bgColor={false} header={'Tralfagar virtual healthcare'} month={'JUN'} day={4} />
+          <Box bgColor={true} header={'Tralfagar virtual healthcare'} month={'JUN'} day={29} url={'Trafalgar'} timeTaken={'4 hours'}
+            figmaLink="https://www.figma.com/file/EWmzcVkd7qbP5Nf7iMvuqP/Trafalgar-Landing-Page" />
+          
+          {/* <Box bgColor={false} />
+          <Box bgColor={true} /> */}
+        
+          {/* <Box bgColor={false} header={'Tralfagar virtual healthcare'} month={'JUN'} day={4} />
           <Box bgColor={true} />
-          <Box bgColor={false} />
+          <Box bgColor={false} /> */}
         </div>
       </div>
     </div>
